98eeb4e75d79632e2a4b39c84a63ebb6f90ade6f
yachoo2606 <janbialy@hotmail.com>
2018-02-08 09:35:14 +0100

    c++

--- zad3.41/main.cpp
+++ zad3.41/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
@@ -11,28 +12,44 @@ struct dane
 struct uczen
 {
     string nazwisko;
+    string imie;
     int numer;
     string klasa;
     dane d;
 }student;
 
-
-
-void wczytaj_statyczne(uczen *student)
+void wczytaj_statyczne(uczen &student)
 {
-    cout<<"Podaj Nazwisko ucznia: ";
+    cout<<"Podaj imie ucznia: ";
+    getline(cin,student.imie);
+    fflush(stdin);
+    cout<<"\nPodaj Nazwisko ucznia: ";
     getline(cin,student.nazwisko);
+    fflush(stdin);
     cout<<"\nPodaj klase ucznia: ";
     cin>>student.klasa;
     cout<<"\nPodaj numer ucznia: ";
     cin>>student.numer;
-    cout<<"\nPodaj miejscowosc i ulice ucznia (miejscowowsc ulica)";
-    getline(cin,student.d.miejscowosc);
+    cout<<"\nPodaj miejscowosc: ";
+    cin>>student.d.miejscowosc;
+    fflush(stdin);
+    cout<<"\nPodaj ulice: ";
     getline(cin,student.d.ulica);
+    fflush(stdin);
+}
+void wypisz_statyczne(uczen &student)
+{
+    cout<<"Imie: "<<student.imie;
+    cout<<"\nNazwisko: "<<student.nazwisko;
+    cout<<"\nKlasa: "<<student.klasa;
+    cout<<"\nNumer: "<<student.numer;
+    cout<<"\nMiejscowowsc: "<<student.d.miejscowosc;
+    cout<<"\nUlica: "<<student.d.ulica;
 }
 int main()
 {
-    wczytaj_statyczne(&student);
+    wczytaj_statyczne(student);
+    wypisz_statyczne(student);
     return 0;
 
 }
